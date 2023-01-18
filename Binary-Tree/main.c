436a0db115997906c63f63d831a4c69845f3e39b
Emir-Brkic <81163301+Emir-Brkic@users.noreply.github.com>
2023-01-18 19:18:47 +0100

    Patched the insert_node funcion, added the new_node function and the depth funcion

--- Binary-Tree/main.c
+++ Binary-Tree/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 // Strict/Proper binary tree
 // each node have either 2 or 0 children
 
@@ -33,68 +34,88 @@
 //Search      O(n)            O(n)          O(log(n))  O(log(n))             
 //Insert      O(1)            O(1)          O(n)       O(log(n))
 //Remove      O(n)            O(n)          O(n)       O(log(n))
-typedef struct node
+typedef struct node_t
 {
     int data;
-    node *l_child;
-    node *r_child;
-}node;
+    struct node *l_child;
+    struct node *r_child;
+}node_t;
 
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+
+node_t *new_node(int val);
+node_t *insert_node(node_t *node, int val);
+int depth(node_t *node);
 
 int main() {
-    node *root;
+    node_t *root;
 
-    root = calloc(1, sizeof(node));
-    root->data = -1;
+    root = new_node(5);
+
     int val = 0;
-    while(1) {
+    printf("Inizio\n");
+    for(int i = 0; i < 3; i ++) {
         
         printf("Inserisci valore : ");
         scanf("%d", &val);
 
-        insert_v(root,val);
+        insert_node(root,val);
 
     }
+
+    printf("\nD = %d", depth(root));
+
+
 
     return 0;
 }
 
-void insert_v(node *node, int val) {
-    if(node->data == -1) {
-        node->data = val;
-        return;
+node_t *new_node(int val) {
+    node_t *new_node;
+    new_node = calloc(1, sizeof(node_t));
+
+    if(new_node == NULL) {
+        printf("Failed to allocate memory!");
+        exit(1);
     }
-    else if(node->data > val) {
-        if(node->l_child != NULL) {
-            insert_v(node->l_child, val);
-            return;
-        }
-        else {
-            node->l_child = calloc(1,sizeof(node));
-            if(node->l_child == NULL) {
-                printf("Failed to allocate memori for a ndoe.");
-                return;
-            }
-            node->l_child->data = -1;
-            insert_v(node->l_child, val);
-            return;
-        }
-    }
-    else {
-        if(node->l_child != NULL) {
-            insert_v(node->r_child, val);
-            return;
-        }
-        else {
-            node->r_child = calloc(1,sizeof(node));
-            if(node->r_child == NULL) {
-                printf("Failed to allocate memori for a ndoe.");
-                return;
-            }
-            node->r_child->data = -1;
-            insert_v(node->r_child, val);
-            return;
-        }
-    }
-    return ;
+
+    new_node->data = val;
+    new_node->l_child = NULL;
+    new_node->r_child = NULL;
+
+    return new_node;
 }
+
+node_t *insert_node(node_t *node, int val) {
+    if(node == NULL) {
+        return new_node(val);
+    }
+    else if(val < node->data){
+        node_t *new_node = insert_node(node->l_child, val);
+        if(new_node != NULL) {
+            node->l_child = new_node;
+        }
+        return NULL;
+    }
+    else if(val > node->data){
+        node_t *new_node = insert_node(node->r_child, val);
+        if(new_node != NULL) {
+            node->r_child = new_node;
+        }
+        return NULL;
+    }
+    else if(val == node->data) {
+        printf("Value already exist in the tree!");
+        return NULL;
+    }
+}
+
+
+
+int depth(node_t *node) { 
+    if(node == NULL) {return 0;}
+    int left = depth(node->l_child);
+    int right = depth(node->r_child);
+    return max(left,right) + 1;
+}
+
