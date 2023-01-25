353f744ecfc338b04a2dfd166adf4e3c3b4737a5
Emir-Brkic <81163301+Emir-Brkic@users.noreply.github.com>
2023-01-25 22:44:02 +0100

    Added the draw free function and started the circle draw

--- Binary-Tree/test_Sdl/src/main.c
+++ Binary-Tree/test_Sdl/src/main.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <stdbool.h>
 #include <SDL.h>
 
@@ -10,6 +11,10 @@
 #define WITH 640
 
 #define DIM_VET (WITH / 10)
+#define PI 3.141592654
+
+void draw_circle(SDL_Renderer *rend);
+void free_draw(SDL_Renderer *rend);
 
 SDL_Window *Inti_wind();
 SDL_Renderer *Init_rend(SDL_Window *wind);
@@ -18,10 +23,16 @@ void Init_rect(struct SDL_Rect *rect);
 void sdl_event_handler();
 void sdl_draw_elements(SDL_Renderer *rend);
 
+// I know that u shouldn't use global variable like this but it is just an exercise
+bool run = true;
+bool fullscreen = false;
+bool sx_mouse_down = false;
+bool dx_moust_down = false;
 
 
-
-bool run = true, fullscreen = false;
+int x1_ = -1;
+int y1_ = -1;
+int x2_, y2_;
 
 int main(int argv, char** args) {
     SDL_Window *wind;
@@ -43,6 +54,42 @@ int main(int argv, char** args) {
     SDL_Quit();
 
     return 0;
+}
+
+void draw_circle(SDL_Renderer *rend) {
+    SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+    /*for(double i = 0; i < 360; i++) {
+        int an = (i * PI) / 180;
+        SDL_RenderDrawPoint(rend,(int)cos((double)an) + 200,(int)sin((double)an) +200);
+    }*/
+}
+
+void free_draw(SDL_Renderer *rend) {
+    if(sx_mouse_down) {
+        SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+
+
+        Uint32 res;
+        res = SDL_GetMouseState( &x2_, &y2_);
+
+
+        if(SDL_BUTTON(res) == 8){
+            SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+            SDL_RenderClear(rend);    
+        }
+        else if(x1_ == -1 && y1_ == -1) {
+            SDL_RenderDrawLine(rend, x2_, y2_, x2_, y2_);
+        }else {
+            res = SDL_RenderDrawLine(rend, x1_, y1_, x2_, y2_);
+        }
+        
+
+        x1_ = x2_;
+        y1_ = y2_;
+    }else {
+        x1_ = -1;
+        y1_ = -1;
+    }
 }
 
 SDL_Window *Inti_wind() {
@@ -67,14 +114,15 @@ SDL_Renderer *Init_rend(SDL_Window *wind) {
 
 
 void sdl_draw_elements(SDL_Renderer *rend) {
-    SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
-    SDL_RenderClear(rend);
+    //SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+    //SDL_RenderClear(rend);
 
-    SDL_SetRenderDrawColor(rend, 40,43,200,255);
-    for(int i = 0; i < DIM_VET; i++) {
-        SDL_RenderFillRect(rend, &re_vet[i]);
-    }
 
+    
+    free_draw(rend);
+    printf("here\n");
+    draw_circle(rend);
+    printf("here\n");
 
     SDL_RenderPresent(rend);
     SDL_Delay(16);
@@ -92,11 +140,21 @@ void sdl_event_handler() {
             break;
 
         case SDL_KEYDOWN:
-
-            if(ev.key.keysym.sym == SDLK_RETURN) {
-                bab();
-            }
             
+            break;
+
+        case SDL_MOUSEBUTTONDOWN:
+            sx_mouse_down = true;
+            dx_moust_down = true;
+
+            
+            break;
+
+        case SDL_MOUSEBUTTONUP:
+            sx_mouse_down = dx_moust_down = false;
+            break;
+
+        case SDL_MOUSEMOTION:
             break;
 
     
@@ -108,4 +166,3 @@ void sdl_event_handler() {
     const Uint8 *keystates = SDL_GetKeyboardState(NULL);
 
 }
-
